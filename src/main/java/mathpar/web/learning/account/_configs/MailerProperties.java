b3fef72e933c1ed9b1f46088211d23eca3861b91
Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
2020-04-19 12:16:42 +0300

    Added remove image goal to build process; Added tls to mail properties

--- src/main/java/mathpar/web/learning/account/_configs/MailerProperties.java
+++ src/main/java/mathpar/web/learning/account/_configs/MailerProperties.java
@@ -7,16 +7,22 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailerProperties {
     @Bean
     @Profile("!test")
     public JavaMailSender mailSender(MathparProperties properties){
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
         var mailSender = new JavaMailSenderImpl();
         mailSender.setHost(properties.getMailHost());
         mailSender.setPort(properties.getMailPort());
         mailSender.setUsername(properties.getMailUsername());
         mailSender.setPassword(properties.getMailPassword());
+        mailSender.setJavaMailProperties(props);
         return mailSender;
     }
 }
