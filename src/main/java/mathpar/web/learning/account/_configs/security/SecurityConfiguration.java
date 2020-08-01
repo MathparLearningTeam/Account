95cc37a74513e71e6f78ac33539fc7e09acec302
MathparLearning <61123042+MathparLearning@users.noreply.github.com>
2020-08-01 16:51:44 +0300

    Account service v0.12.0 release (#14)
    * [ACC-2] Exposed healthcheck endpoint (#11) (#12)
    
    Co-authored-by: Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
    
    * [ACC-1] Added spring security; Secured getAccount (#13)
    
    * [ACC-1] Implemented spring security mechanism and added authentication requirement to getAccount endpoint
    
    * [ACC-1] Removed unnecessary package comments
    
    * [ACC-1] Small naming change to tokens in TokenValidationFilter
    
    Co-authored-by: Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>

--- src/main/java/mathpar/web/learning/account/_configs/security/SecurityConfiguration.java
+++ src/main/java/mathpar/web/learning/account/_configs/security/SecurityConfiguration.java
@@ -1,10 +1,13 @@
-package mathpar.web.learning.account._configs;
+package mathpar.web.learning.account._configs.security;
 
+import mathpar.web.learning.account.services.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.ConcurrentSessionFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,16 +16,26 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final TokenValidationFilter tokenFilter;
+
+    public SecurityConfiguration(TokenService tokenService) {
+        this.tokenFilter = new TokenValidationFilter(tokenService);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.logout().disable();
+        http.anonymous().disable();
+        http.sessionManagement().disable();
+        http.addFilterAfter(tokenFilter, ConcurrentSessionFilter.class);
         http.csrf().disable();
         http.cors();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
