574a5e3e57bf7640cbe3214354d0b0c7707baa45
Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
2020-08-02 15:41:40 +0300

    [ACC-3] Added private getAccountById endpoint

--- src/main/java/mathpar/web/learning/account/controllers/PrivateAccountController.java
+++ src/main/java/mathpar/web/learning/account/controllers/PrivateAccountController.java
@@ -2,6 +2,7 @@ package mathpar.web.learning.account.controllers;
 
 import mathpar.web.learning.account.services.AccountService;
 import mathpar.web.learning.account.utils.dto.payloads.CreateTemporaryCredentialsPayload;
+import mathpar.web.learning.account.utils.dto.responses.AccountResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,11 @@ public class PrivateAccountController {
     @GetMapping(GET_ACCOUNT_ID)
     public long getAccountId(@RequestParam("email") String email){
         return accountService.getAccount(email).orElseThrow().getId();
+    }
+
+    @GetMapping("/account")
+    public AccountResponse getAccountById(@RequestParam("id") long id){
+        return new AccountResponse(accountService.getAccount(id).orElseThrow());
     }
 
     @PostMapping(CREATE_TEMPORARY_ACCOUNT_URL)
